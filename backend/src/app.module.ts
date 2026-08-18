96ba93ca4d1cc7c85b3f37d523e84cad2378b7db
Mohammed Waleed <aiengineer77@icloud.com>
2026-08-18 20:33:58 +0300

    Register customer API services

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,8 @@ import { AppService } from './app.service';
 import { AuthGuard } from './auth/auth.guard';
 import { AuthService } from './auth/auth.service';
 import { RolesGuard } from './auth/roles';
+import { CustomersController } from './customers/customers.controller';
+import { CustomersService } from './customers/customers.service';
 import { ErpController } from './erp/erp.controller';
 import { ErpService } from './erp/erp.service';
 import { InvoicesController } from './invoices/invoices.controller';
@@ -13,7 +15,7 @@ import { ProductsService } from './products/products.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, ErpController, InvoicesController, ProductsController],
-  providers: [AppService, AuthService, AuthGuard, RolesGuard, ErpService, InvoicesService, ProductsService],
+  controllers: [AppController, ErpController, InvoicesController, ProductsController, CustomersController],
+  providers: [AppService, AuthService, AuthGuard, RolesGuard, ErpService, InvoicesService, ProductsService, CustomersService],
 })
 export class AppModule {}
